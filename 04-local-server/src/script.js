33db5545e0efca0a3eaff460486ab121b70987b7
“Shaon <shaon@moneynmore.com>
2023-04-22 15:26:28 +0530

    groups

--- 04-local-server/src/script.js
+++ 04-local-server/src/script.js
@@ -3,29 +3,37 @@ import * as THREE from 'three'
 // Scene
 const scene = new THREE.Scene()
 
-// Object
-const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
-const mesh = new THREE.Mesh(geometry, material)
+const group = new THREE.Group()
+group.position.y = 1
+group.scale.y = .3
+group.rotateY(-1)
+scene.add(group)
 
-// mesh.position.x = 0.6
-// mesh.position.y = -0.3
-// mesh.position.z = 1
-mesh.position.set(0.7,0.6,1)
+const cube1 = new THREE.Mesh(
+    new THREE.BoxGeometry(1,1,1),
+    new THREE.MeshBasicMaterial({color: 'red'})
+)
+group.add(cube1)
+
+const cube2 = new THREE.Mesh(
+    new THREE.BoxGeometry(1,1,1),
+    new THREE.MeshBasicMaterial({color: 'blue'})
+)
+cube2.position.x=2
+group.add(cube2)
+
+const cube3 = new THREE.Mesh(
+    new THREE.BoxGeometry(1,1,1),
+    new THREE.MeshBasicMaterial({color: 'green'})
+)
+cube3.position.y=3
+group.add(cube3)
+
 
 //Axes helper
 const axesHelper = new THREE.AxesHelper(2)
 
-//Scale
-mesh.scale.set(0.4,0.5,1)
-
-//Rotation
-mesh.rotation.reorder('ZYX')
-mesh.rotation.y = 2
-mesh.rotateZ(2)
-
 scene.add(axesHelper)
-scene.add(mesh)
 
 
 // Sizes
@@ -40,9 +48,6 @@ camera.position.z = 3
 camera.position.x = 1
 camera.position.y = 1
 scene.add(camera)
-
-// camera.lookAt(new THREE.Vector3(2,0,0))
-camera.lookAt(mesh.position)
 
 // Renderer
 const renderer = new THREE.WebGLRenderer({
