7d069cb5e1c53139bbcc60040ea42b5110dc0b22
Shaon Baidya <kenkaneki0625@gmail.com>
2023-04-22 16:23:24 +0530

    Lesson6 Animations (#3)

--- 04-local-server/src/script.js
+++ 04-local-server/src/script.js
@@ -1,4 +1,5 @@
 import * as THREE from 'three'
+import gsap from 'gsap';
 
 // Scene
 const scene = new THREE.Scene()
@@ -54,4 +55,16 @@ const renderer = new THREE.WebGLRenderer({
     canvas: document.querySelector('canvas.webgl')
 })
 renderer.setSize(sizes.width, sizes.height)
-renderer.render(scene, camera)
+
+gsap.to(group.position, {duration: 1, delay: 1, x: 2})
+gsap.to(group.position, {duration: 1, delay: 2, x: 0})
+
+//Animations
+const tick = () => {
+
+    //render
+    renderer.render(scene, camera)
+
+    window.requestAnimationFrame(tick)
+}
+tick()
