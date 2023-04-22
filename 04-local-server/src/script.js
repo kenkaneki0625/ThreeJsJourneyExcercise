d727548bbb503862b631d8a55e789a37bccf6df4
Shaon Baidya <kenkaneki0625@gmail.com>
2023-04-22 15:31:41 +0530

    Lesson5 (#2)

--- 04-local-server/src/script.js
+++ 04-local-server/src/script.js
@@ -3,11 +3,38 @@ import * as THREE from 'three'
 // Scene
 const scene = new THREE.Scene()
 
-// Object
-const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
-const mesh = new THREE.Mesh(geometry, material)
-scene.add(mesh)
+const group = new THREE.Group()
+group.position.y = 1
+group.scale.y = .3
+group.rotateY(-1)
+scene.add(group)
+
+const cube1 = new THREE.Mesh(
+    new THREE.BoxGeometry(1,1,1),
+    new THREE.MeshBasicMaterial({color: 'red'})
+)
+group.add(cube1)
+
+const cube2 = new THREE.Mesh(
+    new THREE.BoxGeometry(1,1,1),
+    new THREE.MeshBasicMaterial({color: 'blue'})
+)
+cube2.position.x=2
+group.add(cube2)
+
+const cube3 = new THREE.Mesh(
+    new THREE.BoxGeometry(1,1,1),
+    new THREE.MeshBasicMaterial({color: 'green'})
+)
+cube3.position.y=3
+group.add(cube3)
+
+
+//Axes helper
+const axesHelper = new THREE.AxesHelper(2)
+
+scene.add(axesHelper)
+
 
 // Sizes
 const sizes = {
@@ -18,6 +45,8 @@ const sizes = {
 // Camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
 camera.position.z = 3
+camera.position.x = 1
+camera.position.y = 1
 scene.add(camera)
 
 // Renderer
