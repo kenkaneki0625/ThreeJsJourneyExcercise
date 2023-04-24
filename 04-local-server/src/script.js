93a633209ffda71f586ec85b82f8aed62ee70d97
“Shaon <shaon@moneynmore.com>
2023-04-24 23:08:56 +0530

    orbit controls

--- 04-local-server/src/script.js
+++ 04-local-server/src/script.js
@@ -1,5 +1,5 @@
 import * as THREE from 'three'
-import gsap from 'gsap';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 
 // Scene
 const scene = new THREE.Scene()
@@ -8,6 +8,8 @@ const scene = new THREE.Scene()
 const cursor = {
     x:0, y:0
 }
+
+const canvas = document.querySelector('canvas.webgl')
 
 window.addEventListener('mousemove', (event) =>{
     cursor.x = event.clientX / sizes.width - 0.5
@@ -38,15 +40,22 @@ const sizes = {
 
 // Camera
 const camera = new THREE.PerspectiveCamera(100, sizes.width / sizes.height)
-camera.position.z = 2
+camera.position.z = 3
 // camera.position.x = 2
 // camera.position.y = 2
 camera.lookAt(group.position)
 scene.add(camera)
 
+// Controls
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true
+// controls.target.y = 1
+// controls.update()
+
+
 // Renderer
 const renderer = new THREE.WebGLRenderer({
-    canvas: document.querySelector('canvas.webgl')
+    canvas: canvas
 })
 renderer.setSize(sizes.width, sizes.height)
 
@@ -55,19 +64,8 @@ const clock = new THREE.Clock()
 //Animations
 const tick = () => {
     const elapsedTime = clock.getElapsedTime()
+    controls.update()
     
-    // group.rotation.y = elapsedTime
-
-        // camera.position.x = cursor.x * 3
-        // camera.position.y = cursor.y * 3
-        // camera.lookAt(new THREE.Vector3())
-
-        camera.position.x = Math.sin(cursor.x * Math.PI * 2) *3
-        camera.position.z = Math.cos(cursor.x * Math.PI * 2) *3
-        camera.position.y = cursor.y * 3
-
-
-        camera.lookAt(group.position)
 
     //render
     renderer.render(scene, camera)
