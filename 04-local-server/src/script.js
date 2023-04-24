0babc33c38bb91928118096e22231d83b790daf9
Shaon Baidya <kenkaneki0625@gmail.com>
2023-04-24 23:13:04 +0530

    camera (#4)

--- 04-local-server/src/script.js
+++ 04-local-server/src/script.js
@@ -1,34 +1,29 @@
 import * as THREE from 'three'
-import gsap from 'gsap';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 
 // Scene
 const scene = new THREE.Scene()
 
+//cursor
+const cursor = {
+    x:0, y:0
+}
+
+const canvas = document.querySelector('canvas.webgl')
+
+window.addEventListener('mousemove', (event) =>{
+    cursor.x = event.clientX / sizes.width - 0.5
+    cursor.y = -(event.clientY / sizes.height - 0.5)
+})
+
 const group = new THREE.Group()
-group.position.y = 1
-group.scale.y = .3
-group.rotateY(-1)
 scene.add(group)
 
 const cube1 = new THREE.Mesh(
-    new THREE.BoxGeometry(1,1,1),
+    new THREE.BoxGeometry(1,1,1,5,5,5),
     new THREE.MeshBasicMaterial({color: 'red'})
 )
 group.add(cube1)
-
-const cube2 = new THREE.Mesh(
-    new THREE.BoxGeometry(1,1,1),
-    new THREE.MeshBasicMaterial({color: 'blue'})
-)
-cube2.position.x=2
-group.add(cube2)
-
-const cube3 = new THREE.Mesh(
-    new THREE.BoxGeometry(1,1,1),
-    new THREE.MeshBasicMaterial({color: 'green'})
-)
-cube3.position.y=3
-group.add(cube3)
 
 
 //Axes helper
@@ -44,23 +39,33 @@ const sizes = {
 }
 
 // Camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
+const camera = new THREE.PerspectiveCamera(100, sizes.width / sizes.height)
 camera.position.z = 3
-camera.position.x = 1
-camera.position.y = 1
+// camera.position.x = 2
+// camera.position.y = 2
+camera.lookAt(group.position)
 scene.add(camera)
+
+// Controls
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true
+// controls.target.y = 1
+// controls.update()
+
 
 // Renderer
 const renderer = new THREE.WebGLRenderer({
-    canvas: document.querySelector('canvas.webgl')
+    canvas: canvas
 })
 renderer.setSize(sizes.width, sizes.height)
 
-gsap.to(group.position, {duration: 1, delay: 1, x: 2})
-gsap.to(group.position, {duration: 1, delay: 2, x: 0})
+const clock = new THREE.Clock()
 
 //Animations
 const tick = () => {
+    const elapsedTime = clock.getElapsedTime()
+    controls.update()
+    
 
     //render
     renderer.render(scene, camera)
