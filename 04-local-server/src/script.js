b6103c0f76a90df20c92b7466225cda363174353
“Shaon <shaon@moneynmore.com>
2023-05-12 23:25:28 +0530

    Lights

--- 04-local-server/src/script.js
+++ 04-local-server/src/script.js
@@ -1,25 +1,103 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
-import {TextGeometry} from 'three/examples/jsm/geometries/TextGeometry'
-import { FontLoader } from 'three/examples/jsm/loaders/FontLoader'
-
-const textureLoader = new THREE.TextureLoader()
-const matcapTexture = textureLoader.load('/matcaps/8.png')
-
-const fontLoader = new FontLoader()
-
+import * as dat from 'lil-gui'
+import { RectAreaLightHelper } from 'three/examples/jsm/helpers/RectAreaLightHelper.js'
 
 /**
  * Base
  */
+// Debug
+const gui = new dat.GUI()
+
 // Canvas
 const canvas = document.querySelector('canvas.webgl')
 
 // Scene
 const scene = new THREE.Scene()
 
-// const axesHelper = new THREE.AxesHelper()
-// scene.add(axesHelper)
+/**
+ * Lights
+ */
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.5)
+scene.add(ambientLight)
+gui.add(ambientLight, 'intensity').min(0).max(1).step(0.01).name('ambientLightIntensity')
+
+const directionalLight = new THREE.DirectionalLight(0x00fffc, 0.3)
+directionalLight.position.set(1, 0.25, 0)
+scene.add(directionalLight)
+gui.add(directionalLight, 'intensity').min(0).max(1).step(0.01).name('directionalLightIntensity')
+
+const hemisphereLight = new THREE.HemisphereLight('red', 'purple', 0.3)
+scene.add(hemisphereLight)
+gui.add(hemisphereLight, 'intensity').min(0).max(1).step(0.01).name('hemisphereLightIntensity')
+
+const pointLight = new THREE.PointLight(0xffffff, 0.5, 3)
+// pointLight.position.set(1,-0.5,1)
+pointLight.position.set(2,3,4)
+scene.add(pointLight)
+gui.add(pointLight, 'intensity').min(0).max(1).step(0.01).name('pointLightIntensity')
+
+
+const rectAreaLight = new THREE.RectAreaLight('blue', 2,3,1)
+scene.add(rectAreaLight)
+gui.add(rectAreaLight, 'intensity').min(0).max(5).step(0.01).name('rectAreaLightIntensity')
+
+
+const spotLight = new THREE.SpotLight('yellow', 0.5,6,Math.PI*0.1, 0.25, 1)
+spotLight.position.set(0,2,3)
+scene.add(spotLight)
+gui.add(spotLight, 'intensity').min(0).max(5).step(0.01).name('spotLightIntensity')
+
+
+//helpers
+const hemisphereLightHelper = new THREE.HemisphereLightHelper(hemisphereLight, 0.2)
+scene.add(hemisphereLightHelper)
+
+const directionalLightHelper = new THREE.DirectionalLightHelper(directionalLight, 0.2)
+scene.add(directionalLightHelper)
+
+const pointLightHelper = new THREE.PointLightHelper(pointLight, 0.2)
+scene.add(pointLightHelper)
+
+const spotLightHelper = new THREE.SpotLightHelper(spotLight)
+scene.add(spotLightHelper)
+
+const rectAreaLightHelper = new RectAreaLightHelper(rectAreaLight)
+scene.add(rectAreaLightHelper)
+
+/**
+ * Objects
+ */
+// Material
+const material = new THREE.MeshStandardMaterial()
+material.roughness = 0.4
+
+// Objects
+const sphere = new THREE.Mesh(
+    new THREE.SphereGeometry(0.5, 32, 32),
+    material
+)
+sphere.position.x = - 1.5
+
+const cube = new THREE.Mesh(
+    new THREE.BoxGeometry(0.75, 0.75, 0.75),
+    material
+)
+
+const torus = new THREE.Mesh(
+    new THREE.TorusGeometry(0.3, 0.2, 32, 64),
+    material
+)
+torus.position.x = 1.5
+
+const plane = new THREE.Mesh(
+    new THREE.PlaneGeometry(5, 5),
+    material
+)
+plane.rotation.x = - Math.PI * 0.5
+plane.position.y = - 0.65
+
+scene.add(sphere, cube, torus, plane)
 
 /**
  * Sizes
@@ -44,54 +122,14 @@ window.addEventListener('resize', () =>
     renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 })
 
-fontLoader.load(
-    '/fonts/helvetiker_regular.typeface.json',
-    (font)=>{
-        const textGeometry = new TextGeometry(
-            'Sneha is best bunu',
-            {
-                font, size: 0.5, height:0.2,curveSegments:5,
-                bevelEnabled:true, bevelThickness:0.03, bevelSize:0.02, bevelOffset:0,
-                bevelSegments:3
-            }
-        )
-        // textGeometry.computeBoundingBox()
-        // textGeometry.translate(
-        //     - textGeometry.boundingBox.max.x *0.5,
-        //     - textGeometry.boundingBox.max.y *0.5,
-        //     - textGeometry.boundingBox.max.z *0.5,
-        // )
-        textGeometry.center()
-        const material = new THREE.MeshMatcapMaterial({matcap:matcapTexture})
-        const text = new THREE.Mesh(textGeometry,material)
-        scene.add(text)
-
-        const donutGeometry = new THREE.TorusGeometry(0.3,0.2,20,45)
-            
-
-        for(let i=0;i<100;i++){
-            const donut = new THREE.Mesh(donutGeometry, material)
-
-            donut.position.x = (Math.random() - 0.5)*10
-            donut.position.y = (Math.random() - 0.5)*10
-            donut.position.z = (Math.random() - 0.5)*10
-            donut.rotation.x = Math.random() * Math.PI
-            donut.rotation.y = Math.random() * Math.PI
-
-            const scale = Math.random()
-            donut.scale.set(scale,scale,scale)
-            scene.add(donut)
-        }
-    }
-)
-
-
 /**
  * Camera
  */
 // Base camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
-camera.position.z = 3
+camera.position.x = 1
+camera.position.y = 1
+camera.position.z = 2
 scene.add(camera)
 
 // Controls
@@ -116,6 +154,15 @@ const tick = () =>
 {
     const elapsedTime = clock.getElapsedTime()
 
+    // Update objects
+    sphere.rotation.y = 0.1 * elapsedTime
+    cube.rotation.y = 0.1 * elapsedTime
+    torus.rotation.y = 0.1 * elapsedTime
+
+    sphere.rotation.x = 0.15 * elapsedTime
+    cube.rotation.x = 0.15 * elapsedTime
+    torus.rotation.x = 0.15 * elapsedTime
+
     // Update controls
     controls.update()
 
@@ -125,4 +172,5 @@ const tick = () =>
     // Call tick again on the next frame
     window.requestAnimationFrame(tick)
 }
+
 tick()
