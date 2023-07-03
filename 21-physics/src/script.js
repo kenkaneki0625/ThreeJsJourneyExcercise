2647ecaae335e9415361d20093524d73d5f59fbf
“Shaon <shaon@moneynmore.com>
2023-07-03 19:42:20 +0530

    performance

--- 21-physics/src/script.js
+++ 21-physics/src/script.js
@@ -57,6 +57,8 @@ const environmentMapTexture = cubeTextureLoader.load([
 //physics world
 
 const world = new CANNON.World()
+world.broadphase = new CANNON.SAPBroadphase(world)
+world.allowSleep = true
 world.gravity.set(0,-9.82, 0)
 
 
